d5968fc8b3f976dd83d452a0ea238959eefbd235
ICodeMyOwnLife <icodemyownlife@gmail.com>
2016-04-24 21:12:08 +0700

    Add IdModelControl

---
+++ CB.WPF.Controls.DataControls/IdModelControl.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+
+namespace CB.WPF.Controls.DataControls
+{
+    public class IdModelControl: ContentControl
+    {
+        #region  Constructors & Destructor
+        static IdModelControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(IdModelControl),
+                new FrameworkPropertyMetadata(typeof(IdModelControl)));
+        }
+        #endregion
+    }
+}
