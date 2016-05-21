159991a8bcfa41b39e9ddf61d52cddb03560e47f
ICodeMyOwnLife <icodemyownlife@gmail.com>
2016-05-21 12:00:31 +0700

    Add FileTransferProgressControl

---
+++ CB.Wpf.UserControls/FileTransferProgressControl.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CB.Wpf.UserControls
+{
+    public partial class FileTransferProgressControl
+    {
+        #region  Constructors & Destructor
+        public FileTransferProgressControl()
+        {
+            InitializeComponent();
+        }
+        #endregion
+    }
+}
---
+++ CB.Wpf.UserControls/ViewModels/FileTransferProgressViewModel.cs
@@ -0,0 +1,11 @@
+﻿using CB.Model.Common;
+using CB.Model.Prism;
+
+
+namespace CB.Wpf.UserControls
+{
+    public class FileTransferProgressViewModel: PrismViewModelBase
+    {
+        public FileProgressReporter ProgressReporter { get; set; }
+    }
+}
