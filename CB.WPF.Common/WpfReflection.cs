093ee18400aa6024078c18240bda567a2d9146a7
ICodeMyOwnLife <icodemyownlife@gmail.com>
2016-06-10 07:04:46 +0700

    Edit WpfReflection

--- CB.WPF.Common/WpfReflection.cs
+++ CB.WPF.Common/WpfReflection.cs
@@ -13,14 +13,20 @@ namespace CB.WPF.Common
             => GetDependencyProperty(dependencyObject.GetType(), propertyName);
 
         public static DependencyProperty GetDependencyProperty(this Type elementType, string propertyName)
-            => elementType.GetField(propertyName, BindingFlags.Static | BindingFlags.Public)?.GetValue(null) as
+            => GetDependencyObjectField(elementType, propertyName)?.GetValue(null) as
                DependencyProperty;
 
         public static RoutedEvent GetRoutedEvent(this IReflect elementType, string eventName)
-            => elementType.GetField(eventName, BindingFlags.Static | BindingFlags.Public)?.GetValue(null) as RoutedEvent;
+            => GetDependencyObjectField(elementType, eventName)?.GetValue(null) as RoutedEvent;
 
         public static RoutedEvent GetRoutedEvent(this UIElement element, string eventName)
             => GetRoutedEvent(element.GetType(), eventName);
+        #endregion
+
+
+        #region Implementation
+        private static FieldInfo GetDependencyObjectField(IReflect elementType, string eventName)
+            => elementType.GetField(eventName, BindingFlags.FlattenHierarchy | BindingFlags.Public | BindingFlags.Static);
         #endregion
     }
 }
