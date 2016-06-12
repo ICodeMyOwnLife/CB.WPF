82604df88801b78540625fffc03c6fb850616446
ICodeMyOwnLife <icodemyownlife@gmail.com>
2016-06-12 18:06:15 +0700

    Small edit

--- CB.Wpf.UserControls/ViewModels/FileTransferProgressViewModel.cs
+++ CB.Wpf.UserControls/ViewModels/FileTransferProgressViewModel.cs
@@ -2,13 +2,14 @@
 using System.Windows.Input;
 using CB.Model.Common;
 using CB.Model.Prism;
+using CB.Prism.Interactivity;
 using Microsoft.Practices.Prism.Commands;
 using Prism.Interactivity.InteractionRequest;
 
 
 namespace CB.Wpf.UserControls
 {
-    public class FileTransferProgressViewModel: PrismViewModelBase, IConfirmation
+    public class FileTransferProgressViewModel: PrismViewModelBase, IConfirmContext, IConfirmation
     {
         #region Fields
         private FileProgressState _state = FileProgressState.Running;
@@ -18,26 +19,30 @@ namespace CB.Wpf.UserControls
         #region  Constructors & Destructor
         public FileTransferProgressViewModel()
         {
-            CancelCommand = new DelegateCommand(Cancel);
-            PauseCommand = new DelegateCommand(Pause);
-            ResumeCommand = new DelegateCommand(Resume);
+            CancelCommand = new DelegateCommand(Cancel, () => CanCancel);
+            PauseCommand = new DelegateCommand(Pause, () => CanPause);
+            ResumeCommand = new DelegateCommand(Resume, () => CanResume);
         }
         #endregion
 
 
-        #region  Properties & Indexers
-        public bool CanCancel => CancelAction != null;
-        public virtual Action CancelAction { get; set; }
+        #region  Commands
         public ICommand CancelCommand { get; }
+        public ICommand PauseCommand { get; }
+        public ICommand ResumeCommand { get; }
+        #endregion
+
+
+        #region  Properties & Indexers
+        public bool CanCancel => CancelAction != null && State != FileProgressState.Canceled;
+        public virtual Action CancelAction { get; set; }
         public bool CanPause => PauseAction != null && State == FileProgressState.Running;
         public bool CanResume => ResumeAction != null && State == FileProgressState.Pausing;
         public bool Confirmed { get; set; }
         public object Content { get; set; }
         public virtual Action PauseAction { get; set; }
-        public ICommand PauseCommand { get; }
         public virtual FileProgressReporter ProgressReporter { get; set; }
         public virtual Action ResumeAction { get; set; }
-        public ICommand ResumeCommand { get; }
 
         public FileProgressState State
         {
@@ -47,11 +52,13 @@ namespace CB.Wpf.UserControls
                 if (SetProperty(ref _state, value))
                 {
                     NotifyPropertiesChanged(nameof(CanCancel), nameof(CanPause), nameof(CanResume));
+                    RaiseCommandsCanExecuteChanged(CancelCommand, PauseCommand, ResumeCommand);
                 }
             }
         }
 
         public string Title { get; set; }
+        public WindowRequest WindowRequest { get; } = new WindowRequest();
         #endregion
 
 
@@ -60,8 +67,9 @@ namespace CB.Wpf.UserControls
         {
             if (!CanCancel) return;
 
-            CancelAction();
             State = FileProgressState.Canceled;
+            CancelAction();
+            WindowRequest.Raise(WindowRequestAction.Close);
         }
 
         public void Pause()
