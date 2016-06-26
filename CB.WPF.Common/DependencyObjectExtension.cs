c51ef149dcb89c85a12104bbd58f1491f1372b40
ICodeMyOwnLife <icodemyownlife@gmail.com>
2016-06-26 21:27:40 +0700

    Add DependencyObjectExtension

---
+++ CB.WPF.Common/DependencyObjectExtension.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Windows;
+using System.Windows.Media;
+
+
+namespace CB.WPF.Common
+{
+    public static class DependencyObjectExtension
+    {
+        #region Dependency Properties
+        public static T GetChild<T>(this DependencyObject obj) where T: DependencyObject
+        {
+            var t = obj as T;
+            if (t != null) return t;
+
+            for (var i = 0; i < VisualTreeHelper.GetChildrenCount(obj); i++)
+            {
+                var child = VisualTreeHelper.GetChild(obj, i);
+                var result = GetChild<T>(child);
+                if (result != null) return result;
+            }
+            return null;
+        }
+
+        public static DependencyObject GetChild(this DependencyObject obj, Type childType)
+        {
+            var t = obj.GetType();
+            if (t == childType || t.IsSubclassOf(childType)) return obj;
+
+            for (var i = 0; i < VisualTreeHelper.GetChildrenCount(obj); i++)
+            {
+                var child = VisualTreeHelper.GetChild(obj, i);
+                var result = GetChild(child, childType);
+                if (result != null) return result;
+            }
+
+            return null;
+        }
+        #endregion
+    }
+}
