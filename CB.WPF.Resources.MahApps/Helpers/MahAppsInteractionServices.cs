2f63cdc88b3d2eba8ea72dae30c13d8c7a10bc44
ICodeMyOwnLife <icodemyownlife@gmail.com>
2016-06-30 07:51:10 +0700

    Add MahAppsInteractionServices

---
+++ CB.WPF.Resources.MahApps/Helpers/MahAppsInteractionServices.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Interactivity;
+using CB.Prism.Interactivity;
+using CB.WPF.MahAppsResources.Windows;
+using Prism.Interactivity.InteractionRequest;
+using TriggerBase = System.Windows.Interactivity.TriggerBase;
+
+
+namespace CB.WPF.MahAppsResources
+{
+    public class MahAppsInteractionServices
+    {
+        #region Dependency Properties
+        private static void RegisterRequestManager(DependencyObject obj, RequestManager requestManager)
+        {
+            var triggers = Interaction.GetTriggers(obj);
+            triggers.Add(CreateConfirmTrigger(requestManager));
+            triggers.Add(CreateNotifyTrigger(requestManager));
+            triggers.Add(CreateFileTrigger(requestManager));
+            triggers.Add(new WindowRequestTrigger { SourceObject = requestManager.WindowRequestProvider.Request });
+        }
+
+        public static readonly DependencyProperty RequestManagerProperty = DependencyProperty.RegisterAttached(
+            "RequestManager", typeof(RequestManager), typeof(MahAppsInteractionServices),
+            new PropertyMetadata(default(RequestManager), OnRequestManagerChanged));
+
+        [Category("MahAppsWindowsServices")]
+        [AttachedPropertyBrowsableForType(typeof(DependencyObject))]
+        public static RequestManager GetRequestManager(DependencyObject d)
+            => (RequestManager)d.GetValue(RequestManagerProperty);
+
+        [Category("MahAppsWindowsServices")]
+        [AttachedPropertyBrowsableForType(typeof(DependencyObject))]
+        public static void SetRequestManager(DependencyObject d, RequestManager value)
+            => d.SetValue(RequestManagerProperty, value);
+
+        private static void UnRegisterRequestManager(DependencyObject obj, RequestManager requestManager)
+        {
+            if (requestManager == null) return;
+
+            //UNDONE!
+        }
+        #endregion
+
+
+        #region Implementation
+        private static TriggerBase CreateConfirmTrigger(RequestManager requestManager)
+        {
+            var confirmTrigger = new InteractionRequestTrigger
+            {
+                SourceObject = requestManager.ConfirmRequestProvider.Request
+            };
+            confirmTrigger.Actions.Add(CreateWindowTriggerAction(typeof(MahAppsConfirmWindow)));
+            return confirmTrigger;
+        }
+
+        private static TriggerBase CreateFileTrigger(RequestManager requestManager)
+        {
+            var fileTrigger = new InteractionRequestTrigger
+            {
+                SourceObject = requestManager.FileRequestProvider.Request
+            };
+            fileTrigger.Actions.Add(CreateWindowTriggerAction(null));
+            return fileTrigger;
+        }
+
+        private static TriggerBase CreateNotifyTrigger(RequestManager requestManager)
+        {
+            var notifyTrigger = new InteractionRequestTrigger
+            {
+                SourceObject = requestManager.NotifyRequestProvider.Request
+            };
+            notifyTrigger.Actions.Add(CreateWindowTriggerAction(typeof(MahAppsNotifyWindow)));
+            return notifyTrigger;
+        }
+
+        private static WindowTriggerAction CreateWindowTriggerAction(Type windowType)
+        {
+            return new WindowTriggerAction
+            {
+                CenterOverAssociatedObject = true,
+                IsModal = true,
+                WindowType = windowType
+            };
+        }
+
+        private static void OnRequestManagerChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            UnRegisterRequestManager(d, (RequestManager)e.OldValue);
+            RegisterRequestManager(d, (RequestManager)e.NewValue);
+        }
+        #endregion
+    }
+}
